184cde930c2a0f506251cc228944e09f9510c764
DaveToCrackCode <205121009@nitt.edu>
2023-03-29 19:23:20 +0530

    advancment

--- script.js
+++ script.js
@@ -1,46 +1,73 @@
 
 function onclickhandle(){
-   console.log("Click")
+  //  console.log("Click")
   var x= document.getElementById("droparray");
-  if(x.style.display=="none"){
-    x.style.display="block";
+  if(x.style.display=="block"){
+    x.style.display="none";
    
   }else{
-    x.style.display="none";
+    x.style.display="block";
   }
 }
+
+
 function onclickhandle1(){
-  console.log("Click")
- var x= document.getElementById("drop1");
- if(x.style.display=="none"){
-   x.style.display="block";
- }else{
+  var x= document.getElementById("sorting");
+  var dis=x.style.display;
+  // console.log("click");
+  if(dis=='block'){
+    // console.log("Click2")
    x.style.display="none";
+  }
+  else {
+  // console.log("Click1")
+   x.style.display="block"; 
  }
 }
+
+function onclickhandle2(){
+  var x=document.getElementById("Searching");
+  
+  if(x.style.display=="block"){
+    x.style.display="none";
+  }
+  else{
+    x.style.display="block";
+  }
+}
+
+function onclickhandle3(){
+  var x=document.getElementById("Recursion");
+  if(x.style.display=="block"){
+    x.style.display="none";
+  }
+  else{
+    x.style.display="block";
+  }
+}
+
+function opendis(){
+  var x=document.getElementById("discriptionblock");
+  console.log("kk");
+  if(x.style.display=="block"){
+    document.getElementById("bubble").style.color="white";
+    
+  x.style.display="none";
+  }
+  else{
+    document.getElementById("bubble").style.color="red";
+    x.style.display="block";
+  }
+}
+
+
 function openNav(){
   console.log("yes");
-  document.getElementById("mySidepanel").style.width = "250px";
+  document.getElementById("mySidepanel").style.width = "300px";
+  document.getElementById("mySidepanel").style.height = "100%";
+  
 }
 
 function closeNav(){
   document.getElementById("mySidepanel").style.width = "0";
 }
-// function myFunction() {
-//     document.getElementById("droparray").classList.toggle("show");
-//   }
-  
-//   // Close the dropdown if the user clicks outside of it
-//   window.onclick = function(event) {
-//     if (!event.target.matches('array')) {
-//       var dropdowns = document.getElementsByClassName("dropdown-content");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
-
