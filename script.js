02af05b9d838268c8754503e3422398e98fbf79d
DaveToCrackCode <205121009@nitt.edu>
2023-05-11 15:56:14 +0530

    upd

--- script.js
+++ script.js
@@ -43,16 +43,21 @@ function changemode1(){
   var ftclr=document.getElementById("footer");
   var sidepan=document.getElementsByClassName("sidepanel");
   var serial_color=document.getElementsByClassName("serial");
-  console.log(serial_color);
+  var time_table_even=document.getElementsByClassName("timetable_even");
+  console.log(time_table_even);
   
   if(localStorage.getItem("mode") == 'black'){
+    for(var i=0;i<time_table_even.length;i++){
+      time_table_even[i].style.backgroundColor="#303030";
+
+    }
     hbcolor.style.backgroundColor="black";
-    ftclr.style.backgroundColor="black"
+    ftclr.style.backgroundColor="black";
     for(var i=0;i<serial_color.length;i++){
       serial_color[i].style.color="white";
      }
     // bk.style.backgroundImage="url(\"images/bbg2.png\")";
-    bk.style.backgroundColor="black";
+    bk.style.backgroundColor="#101010";
     bk.style.color="white";
     for(var i=0;i<y.length;i++){
       y[i].style.color="white";
@@ -65,13 +70,16 @@ function changemode1(){
 }
 
 else{
+  for(var i=0;i<time_table_even.length;i++){
+  time_table_even[i].style.backgroundColor="#DCDCDC";
+  }
   hbcolor.style.backgroundColor="white";
   ftclr.style.backgroundColor="white";
   for(var i=0;i<serial_color.length;i++){
     serial_color[i].style.color="black";
    }
   // bk.style.backgroundImage="url(\"images/bbg1.png\")";
-  bk.style.backgroundColor="white";
+  bk.style.backgroundColor="#F5F5F5";
   bk.style.color="black";
   for(var i=0;i<y.length;i++){
       y[i].style.color="black";
@@ -92,6 +100,7 @@ function changemod(){
   var ftclr=document.getElementById("footer");
   var sidepan=document.getElementsByClassName("sidepanel");
   var serial_color=document.getElementsByClassName("serial");
+  var time_table_even=document.getElementsByClassName("timetable_even");
   console.log(serial_color);
 
  const val =  localStorage.getItem('mode');
@@ -99,10 +108,13 @@ function changemod(){
   // console.log("mode is " , localStorage.getItem("mode"));
   
     if(localStorage.getItem("mode") == 'black'){
+      for(var i=0;i<time_table_even.length;i++){
+      time_table_even[i].style.backgroundColor="#DCDCDC";
+      }
       hbcolor.style.backgroundColor="white";
       ftclr.style.backgroundColor="white";
       // bk.style.backgroundImage="url(\"images/bbg1.png\")";
-      bk.style.backgroundColor="white";
+      bk.style.backgroundColor="#F5F5F5";
       bk.style.color="black";
       for(var i=0;i<serial_color.length;i++){
        serial_color[i].style.color="black";
@@ -117,13 +129,16 @@ function changemod(){
     }
   
   else{
+    for(var i=0;i<time_table_even.length;i++){
+    time_table_even[i].style.backgroundColor="#303030";
+    }
     hbcolor.style.backgroundColor="black";
     ftclr.style.backgroundColor="black"
     for(var i=0;i<serial_color.length;i++){
       serial_color[i].style.color="white";
      }
     // bk.style.backgroundImage="url(\"images/bbg2.png\")";
-    bk.style.backgroundColor="black";
+    bk.style.backgroundColor="#101010";
     bk.style.color="white";
     for(var i=0;i<y.length;i++){
       y[i].style.color="white";
@@ -139,12 +154,13 @@ function changemod(){
   
  }
  else {
-
+   
   localStorage.setItem("mode","white");
+  time_table_even.style.backgroundColor="#303030";
   hbcolor.style.backgroundColor="black";
   ftclr.style.backgroundColor="black";
   // bk.style.backgroundImage="url(\"images/bbg2.png\")";
-  bk.style.backgroundColor="black";
+  bk.style.backgroundColor="#101010";
   bk.style.color="white";
   for(var i=0;i<y.length;i++){
     y[i].style.color="white";
